eeaf222db3d64a9137b91a677a2c8f051c52b4fd
Andrew Wardrop <andrew.wardrop@outlook.com>
2022-03-07 13:53:15 -0500

    assignments
    adding some assignments to folders

---
+++ Week 1/Assignment 1.1 pt 2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    { //Create a simple console application and declare basic datatypes like int, float, decimal, double and print their min and max values.
+        static void Main(string[] args)
+        {
+            Console.Write("Float max value:"); Console.WriteLine(float.MaxValue);
+            Console.Write("Float min value:"); Console.WriteLine(float.MinValue);
+            Console.Write("int max value:"); Console.WriteLine(int.MaxValue);
+            Console.Write("int min value:"); Console.WriteLine(int.MinValue);
+            Console.Write("decimal max value:"); Console.WriteLine(decimal.MaxValue);
+            Console.Write("decimal min value:"); Console.WriteLine(decimal.MinValue);
+            Console.Write("double max value:"); Console.WriteLine(double.MaxValue);
+            Console.Write("double min value:"); Console.WriteLine(double.MinValue);
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
---
+++ Week 2/assignment 2.2 part 1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+        
+            namespace Assignement_2._2
+    {
+    //Write a console application to create a text file and save your basic details like name, age, address ( use dummy data). Read the details from same file and print on console.
+    class Program
+        {
+            static string Filename;
+            static string path = @"C:\Files\";
+            static void Main(string[] args)
+            {
+                Console.WriteLine("This application will create a file to store basic info");
+                Console.WriteLine("enter a file name:");
+                Filename = Console.ReadLine() + ".txt";
+                path = path + Filename;
+                Console.WriteLine("enter your name");
+                string name = Console.ReadLine();
+                Console.WriteLine("enter your age");
+                int age = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("enter your address");
+                string address = Console.ReadLine();
+                Appendlines("\n" + "name:" + name + "age:" + age + "address:" + address);
+                Readfromlog();
+                Console.ReadLine();
+
+
+
+            }
+            static void Readfromlog()
+            {
+                StreamReader stream = new StreamReader(path);
+                string data = stream.ReadToEnd();
+                Console.WriteLine("Contents" + data);
+                stream.Close();
+            }
+            static void Appendlines(string data)
+            {
+                File.AppendAllText(path, data);
+            }
+        }
+    }
+}
