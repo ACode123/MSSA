ee7baada4fd289792107d3e8bd108268978a2035
Andrew Wardrop <andrew.wardrop@outlook.com>
2022-03-29 22:31:30 -0400

    Read desc
    Create MVC core web app to create: “Books Gallery” . UI to display necessary menu links like : Home, About, Books. Click on Books link to display names of books and click on each book to see details of each book ( like photo, author, description) Book model class will also be needed.

---
+++ Week 12/Assignment 12.2/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_12._2.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Titles = new string[] { "Book1", "Book2", "Book3", "UselessBook" };
+            return View();
+        }
+        public IActionResult Details(string Title)
+        {
+            ViewBag.SelectedBook = Title;
+            return View();
+        }
+        public IActionResult GetImage(string Title)
+        {
+            return File($@"\Images\{Title.ToLower()}.jpg", "image/jpeg");
+        }
+    }
+}
